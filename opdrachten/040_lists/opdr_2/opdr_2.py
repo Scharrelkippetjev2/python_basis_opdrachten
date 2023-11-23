7982fd3fbddf82b502731e62d817d7342541a401
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 20:38:30 +0100

    Versie 4.2

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -1,6 +1,6 @@
 # Opdracht 2 lists
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: IT1X
 
 
 rivier_info = {
@@ -10,6 +10,21 @@ rivier_info = {
 }
 
 rivieren = list(rivier_info.keys())
-# rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
-# Hier jouw code.....
+print("deel 1")
+# print de naam van de 1e rivier:
+print(rivieren[0].capitalize())
+#print het 2e land waar de 1e rivier doorheen stroomt met hoofdletter:
+print(rivier_info['rijn'][1].capitalize())
+
+print("\ndeel 2")
+#print naam 2e rivier
+print(rivieren[1].capitalize())
+#print het eerste land waar de eerste rivier doorheen stroomt.
+print(rivier_info['rijn'][0].capitalize())
+
+print("\ndeel 3")
+#print naam 3e rivier
+print(rivieren[2].capitalize())
+#print het derde land waar de eerste rivier doorheen stroomt.
+print(rivier_info['rijn'][2].capitalize())
