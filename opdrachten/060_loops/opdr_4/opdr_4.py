bb557a3e936cd344f8543915655228bd825222ef
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 21:51:44 +0100

    Versie 6.4

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -1,9 +1,13 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Jesse JAnsen
+# Groep: it1x
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
 my_list = []
+
+x = range(1, 10)
+y = [4 * x + 7 for x in x]
+print(y)
