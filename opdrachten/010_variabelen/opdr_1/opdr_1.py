6edae0815764ceb7e8cd80bd3b04551f92707682
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 15:04:00 +0100

    versie 1

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,7 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: IT1X
 
 # Hier komt je code...
-
-my_list = ...
+my_list = '13 \n"wat een weertje" \n12.5 \n8'
+print(my_list)
