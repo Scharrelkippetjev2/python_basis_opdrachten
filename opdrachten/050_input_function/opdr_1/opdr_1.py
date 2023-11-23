22f8da719c491cbd1ddce713e58b8a517dd952f0
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 20:57:46 +0100

    Versie 5.1

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -1,7 +1,13 @@
 # Opdracht 1 input function
-# Naam student:
-# Groep:
+# Naam student:  Jesse JANSEN
+# Groep: ITx1
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+import math
 
+zijde_een = int(input("Geef de lengte van de eerste zijde\n"))
+zijde_twee = int(input("Geef de lengte van de tweede zijde\n"))
 
+antwoord = (math.sqrt((zijde_een * zijde_een) + (zijde_twee * zijde_twee)))
+
+print(f'De lengte van de schuine zijde is: {antwoord}')
