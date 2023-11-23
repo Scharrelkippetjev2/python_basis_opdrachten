5cc0525418bdda9b10f493b04e5d8ba00ba08c8b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 21:16:44 +0100

    Versie 5.3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,13 @@
 
 # Hier komt je code...
 
+stadlijst = []
+maxlengte = 5
+while len(stadlijst) < maxlengte:
+    stad = input("wilt u de volgende stad invoeren: Amsterdam, Zwolle, Dronten, Haarlem, Zaanstad? ")
+    stadlijst.append(stad)
+    print(f'{stadlijst}')
+
+stadlijst.sort(reverse=True, key=lambda stad: stad.lower())
+print('\nGesoorteerd lijstje reverse:')
+print(f'{stadlijst}')
