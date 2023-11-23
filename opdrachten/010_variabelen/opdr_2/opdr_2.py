4e4fd962070be53110ee0f1ee8946ed038ef20f3
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 15:25:44 +0100

    versie 1.2

--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,9 +1,9 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: ITx1
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = '11 \n"Oh een tuple" \n13.5 \n8009'
 
-print()
+print(my_tuple)
