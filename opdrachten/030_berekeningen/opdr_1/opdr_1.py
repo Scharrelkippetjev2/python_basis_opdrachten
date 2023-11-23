b1e07915654a8d1d19806ee70af73979d68ad819
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 17:12:22 +0100

    Versie 3.1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,15 @@
 # Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: IT1X
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+totaal = joe + bob + fred
+gemiddeld = totaal / 3
+print("Dit weten we over joe, bob en fred:")
+print('De totale waarde van alle leeftijden bij elkaar opgeteld = {}'.format(totaal))
+print('De gemiddelde leeftijd van deze boys is: {}'.format(gemiddeld))
