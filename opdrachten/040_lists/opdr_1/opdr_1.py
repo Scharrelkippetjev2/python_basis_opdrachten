ec9ecb430838c035cdf7e0ff4f02c9693bb82ccf
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 20:22:49 +0100

    Versie 3.3

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,23 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: IT1X
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+
+#maakt lijst aan:
+mylist = []
+
+#maak direc aan:
+dict_1 = { "id": 1, "voornaam": "Coen", "achternaam": "Hubert" }
+#omdat python bij 0 begint met tellen en je de 2e naam wil weten schrijf je bij print 1 neer.
+dict_2 = { "id": 2, "voornaam": "Jolijn", "achternaam": "Evert" }
+dict_3 = { "id": 3, "voornaam": "Melle", "achternaam": "Lia" }
+dict_4 = { "id": 4, "voornaam": "Otte", "achternaam": "Jozef" }
+
+#directie toevoiegen aan de lijst:
+mylist.append(dict_1)
+mylist.append(dict_2)
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+#laat lijst zien met de 2e persoon op het scherm
+print(mylist[1]["voornaam"], mylist[1]["achternaam"])
