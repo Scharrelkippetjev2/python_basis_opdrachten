7aeadf51b118085e47b77621e51c657cbd37dc6b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 19:15:56 +0100

    Versie 3.2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,19 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: itx1
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 144
 
-print()
+
+#ik wil in de print hieronder dat de aantal graden berekent word in de print functie
+print(f"{c} graden Celsius is gelijk aan {(c * 1.8) + 32} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {(f - 32) * 5 / 9} graden Celcuis")
+
+C = 62.2
+F = 96
+print('\n')
+print(f"{C} graden Celsius is gelijk aan {(C * 1.8) + 32} graden Fahrenheit")
+print(f"{F} graden Fahrenheit is gelijk aan {(F - 32) * 5 / 9} graden Celsius")
