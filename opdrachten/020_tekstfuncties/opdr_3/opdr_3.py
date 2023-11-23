b093d2e0a6eca99384397920bc034a79c37152f2
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 17:04:22 +0100

    Versie 2.3

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,17 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Naam student: Jesse Jansen
+# Groep: ITx1
 
 # Hier komt je code...
+b1 = "    *"
+b2 = "   ***"
+b3 = "  ******"
+b4 = " ********"
+b5 = "***********"
+b6 = "    ***"
+b7 = "    ***"
+b8 = "    ***"
+
+hele_boom = '{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n\n'.format(b1, b2, b3, b4, b5, b6, b7, b8)
+
+print(hele_boom * 5)
