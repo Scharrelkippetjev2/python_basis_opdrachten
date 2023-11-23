b7b6b3b8318baf859d2db6e5d452efd580b6198e
BuildTools <unconfigured@null.spigotmc.org>
2023-11-23 21:32:49 +0100

    Versie 6.1

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -1,9 +1,13 @@
 # Opdracht 1 loops
-# Naam student:
-# Groep:
+# Naam student: JEsse JAnsen
+# Groep: ITx1
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 # Hier start de for-loop....
 
-my_list = []
+mylist = []
+
+for nummer in range(1, 11):
+ mylist.append(nummer)
+print(mylist)
