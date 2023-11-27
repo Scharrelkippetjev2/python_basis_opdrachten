a26c85ca8838ce916ae6334857b87112f86b73d7
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 20:38:41 +0100

    versie 7.1

--- opdrachten/070_condities/opdr_1/opdr_1.py
+++ opdrachten/070_condities/opdr_1/opdr_1.py
@@ -1,12 +1,14 @@
 # Opdracht 1 condities
-# Naam student:
-# Groep:
+# Naam student: JsJ
+# Groep: itx1
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 # Hier start de for-loop....
 
 my_list = []
-
 #for loop
-#if statement
+for my_list in range(1, 11):
+    #if statement
+    if my_list > 4:
+        print(my_list)
