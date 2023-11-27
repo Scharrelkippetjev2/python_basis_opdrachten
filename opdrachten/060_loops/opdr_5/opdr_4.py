57edd2840c28cd0b40ee400d33f99d4ceb710361
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 19:54:26 +0100

    versie 6.5

--- opdrachten/060_loops/opdr_5/opdr_4.py
+++ opdrachten/060_loops/opdr_5/opdr_4.py
@@ -1,9 +1,12 @@
-# Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Opdracht 5 input functie
+# Naam student: JEsse
+# Groep: itx1
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = [i * 3 for i in range(1, 21)]
+zes = my_list[:6]
+
+print(zes)
