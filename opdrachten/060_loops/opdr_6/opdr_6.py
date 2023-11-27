225d57dedc45d56a5a535e5cdf7e32f82447a382
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 20:27:40 +0100

    versie 6.6

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,26 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: jsj
+# Groep: itx1
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+print(my_list)
+
+alphabet_my_list = sorted(my_list)
+print(alphabet_my_list)
+
+fav_pizza = ['pepperioni']
+my_list.extend(fav_pizza)
+print(my_list)
+
+my_list.remove("quattro stagioni")
+print(my_list)
+
+print(my_list[:3])
+
+print(my_list[2:3])
+
+print(my_list[2:6])
