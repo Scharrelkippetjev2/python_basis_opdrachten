2f9e0b1196017e5c33015a6b5ef15f112e3e29c4
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 22:05:57 +0100

    versie 8.1

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,19 @@
 # Opdracht 1 while-loops
-# Naam student:
-# Groep:
+# Naam student: JsJ
+# Groep: IT1X
 
 # Jouw code komt hier
+prompt = "Wat vind je van de huidige regering? \n" \
+         "Wat vind je van de Python-lessen tot nu toe?\n" \
+         "Wat vind jij de mooiste stad van Nederland?\n"
+
+active = True
+lst = []
+for i in range(3):
+    message = input(prompt)
+    lst.append(message)
+
+fo = open('vragen.txt', "wt")
+for i in lst:
+    fo.write(i + "\n")
+fo.close()
