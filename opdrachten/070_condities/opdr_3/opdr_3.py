d0866501e2278fc21760c4221e89433a74c645da
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 20:48:45 +0100

    versie 7.3

--- opdrachten/070_condities/opdr_3/opdr_3.py
+++ opdrachten/070_condities/opdr_3/opdr_3.py
@@ -1,6 +1,6 @@
 # Opdracht 3 condities
-# Naam student:
-# Groep:
+# Naam student: JsJ
+# Groep: IT1X
 
 
 
@@ -10,3 +10,17 @@ kortings_percentages = {"baby": 100, "kinderen": 50, "volwassenen": 0, "ouderen"
 leeftijd = {"baby": (0, 2), "kinderen": (3, 18), "volwassenen": (19, 64), "ouderen": (65, 150)}
 
 # Hier komt je code...
+
+levensjaar = int(input("Geef u leeftijd in aantal jaar: "))
+
+for groep in leeftijd:
+#Dit stukje controleert of de leeftijd van de bezoeker binnen de leeftijdsgrenzen van de huidige cat valt.
+        if leeftijd[groep][0] <= levensjaar <= leeftijd[groep][1]:
+            korting = kortings_percentages[groep]
+
+            print(f"U behoort tot de groep {groep}")
+            print(f"U krijgt {korting}% korting")
+            
+            toegangsprijs = normale_toegangsprijs * (100 - korting) / 100
+#2f staat voor 2 decimalen achter de komma.
+            print(f"U betaald daarom €{toegangsprijs:.2f}")
