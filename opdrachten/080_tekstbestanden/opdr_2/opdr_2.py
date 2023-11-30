3511b77b49b6c93bd44daaaaeb549df423916c67
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 11:55:27 +0100

    versie 8.2

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -1,10 +1,24 @@
 # Opdracht 2 tekstbestanden
-# Naam student:
-# Groep:
+# Naam student: JsJ
+# Groep: It1x
 
 import random
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
+pogingen = 0
 
+while True:
+    gebruikers_getal = int(input("raad mijn geheime getal: "))
+    pogingen += 1
+    
+    if gebruikers_getal == geheim_getal:
+        print("je hebt het getal geraden!")
+        break
+    elif gebruikers_getal > geheim_getal:
+        print("lager")
+    else:
+        print("hoger")
+print(f"het getal was {geheim_getal}")
+print(f"je hebt het in {pogingen} keer gedaan!")
