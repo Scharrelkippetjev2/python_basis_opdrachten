d7611962535c5d244569f682c25c437a85827392
BuildTools <unconfigured@null.spigotmc.org>
2023-11-30 13:30:56 +0100

    versie opnieuw

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,15 +3,11 @@
 # Groep: ITx1
 
 # Hier komt je code...
-b1 = "    *"
-b2 = "   ***"
-b3 = "  ******"
-b4 = " ********"
-b5 = "***********"
-b6 = "    ***"
-b7 = "    ***"
-b8 = "    ***"
-
-hele_boom = '{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n\n'.format(b1, b2, b3, b4, b5, b6, b7, b8)
-
-print(hele_boom * 5)
+print("    *        " * 5)
+print("   ***       " * 5)
+print("  ******     " * 5)
+print(" ********    " * 5)
+print("***********  " * 5)
+print("    ***      " * 5)
+print("    ***      " * 5)
+print("    ***      " * 5)
